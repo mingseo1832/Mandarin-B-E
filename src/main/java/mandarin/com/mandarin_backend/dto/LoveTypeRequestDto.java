167b82972de7bd2950085cea59a291eb09a38e97
mingseo1832 <jminseo1832@naver.com>
2025-12-04 16:24:42 +0900

    러브타입 DB에 저장 API

---
+++ src/main/java/mandarin/com/mandarin_backend/dto/LoveTypeRequestDto.java
@@ -0,0 +1,11 @@
+package mandarin.com.mandarin_backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoveTypeRequestDto {
+    private Integer loveType;  // 0~15: 실제 타입
+}
+
