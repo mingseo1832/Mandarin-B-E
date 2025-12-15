83a42c6fcde87e7d3e823329a35559135bac22ef
안도현 <dohyun0715dn@gmail.com>
2025-12-15 16:11:59 +0900

    러브타입 json 파일 위치 수정, 컬럼명 일부 수정

--- src/main/java/mandarin/com/mandarin_backend/entity/ChatReportAvg.java
+++ src/main/java/mandarin/com/mandarin_backend/entity/ChatReportAvg.java
@@ -29,7 +29,7 @@ public class ChatReportAvg {
      * FK → User
      */
     @ManyToOne
-    @JoinColumn(name = "id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     /**
